04f8c5ccdec38b5d0469e1780527f049f2e36955
Michel Zinin <mzinin@gmail.com>
2022-09-11 17:15:20 +0200

    Update incoming message structure

--- gwfront/src/handler/json_checker.cpp
+++ gwfront/src/handler/json_checker.cpp
@@ -3,16 +3,72 @@
 
 #include <nlohmann/json.hpp>
 
+#include <optional>
 #include <sstream>
 #include <string_view>
 #include <vector>
 
 namespace
 {
-    // TODO replace with real fields
-    const std::vector<std::string_view> MANDATORY_FIELDS = {
-        "field1", "field2", "field3"
-    };
+    std::optional<JsonChecker::Result> checkIntegerField(const nlohmann::json& json, std::string_view field)
+    {
+        const auto it = json.find(field);
+        if (it == json.end())
+        {
+            std::ostringstream message;
+            message << "field '" << field << "' is missing";
+            return JsonChecker::Result{false, std::move(message).str()};
+        }
+
+        if (!it->is_number_integer())
+        {
+            std::ostringstream message;
+            message << "field '" << field << "' is not an integer";
+            return JsonChecker::Result{false, std::move(message).str()};
+        }
+
+        return {};
+    }
+
+    std::optional<JsonChecker::Result> checkDoubleField(const nlohmann::json& json, std::string_view field)
+    {
+        const auto it = json.find(field);
+        if (it == json.end())
+        {
+            std::ostringstream message;
+            message << "field '" << field << "' is missing";
+            return JsonChecker::Result{false, std::move(message).str()};
+        }
+
+        if (!it->is_number_float() && !it->is_number_integer())
+        {
+            std::ostringstream message;
+            message << "field '" << field << "' is not a double";
+            return JsonChecker::Result{false, std::move(message).str()};
+        }
+
+        return {};
+    }
+
+    std::optional<JsonChecker::Result> checkStringField(const nlohmann::json& json, std::string_view field)
+    {
+        const auto it = json.find(field);
+        if (it == json.end())
+        {
+            std::ostringstream message;
+            message << "field '" << field << "' is missing";
+            return JsonChecker::Result{false, std::move(message).str()};
+        }
+
+        if (!it->is_string())
+        {
+            std::ostringstream message;
+            message << "field '" << field << "' is not a string";
+            return JsonChecker::Result{false, std::move(message).str()};
+        }
+
+        return {};
+    }
 }
 
 JsonChecker::Result JsonChecker::check(const std::string& data)
@@ -25,23 +81,33 @@ JsonChecker::Result JsonChecker::check(const std::string& data)
         return {false, "data is not a valid json"};
     }
 
-    for (const auto& mandatoryField : MANDATORY_FIELDS)
+    for (int i = 0; i < 6; ++i)
     {
-        const auto it = json.find(mandatoryField);
-        if (it == json.end())
+        const auto result = checkIntegerField(json, "int" + std::to_string(i));
+        if (result)
         {
-            std::ostringstream message;
-            message << "field '" << mandatoryField << "' is missing";
-            Log(error) << "JsonChecker: " << message.view();
-            return {false, std::move(message).str()};
+            Log(error) << "JsonChecker: " << result->message;
+            return *result;
         }
+    }
 
-        if (!it->is_string())
+    for (int i = 0; i < 6; ++i)
+    {
+        const auto result = checkDoubleField(json, "double" + std::to_string(i));
+        if (result)
         {
-            std::ostringstream message;
-            message << "field '" << mandatoryField << "' is missing";
-            Log(error) << "JsonChecker: " << message.view();
-            return {false, std::move(message).str()};
+            Log(error) << "JsonChecker: " << result->message;
+            return *result;
+        }
+    }
+
+    for (int i = 0; i < 32; ++i)
+    {
+        const auto result = checkStringField(json, "str" + std::to_string(i));
+        if (result)
+        {
+            Log(error) << "JsonChecker: " << result->message;
+            return *result;
         }
     }
 
